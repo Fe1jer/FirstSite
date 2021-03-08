22b38d4eda67580d6bb24f50beaa13f69e2f839b
Viktor <vppechko@gmail.com>
2021-03-08 21:54:34 +0300

    added password hashing, added courier role and order distribution,

--- WebApplication1/Data/Repository/ShopCartRepository.cs
+++ WebApplication1/Data/Repository/ShopCartRepository.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
 using WebApplication1.Data.Interfaces;
 using WebApplication1.Data.Models;
 using Microsoft.EntityFrameworkCore;
