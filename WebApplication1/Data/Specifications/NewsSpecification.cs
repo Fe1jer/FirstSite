ea1dd1a2cda29342b5339f482ed661d909ee32cf
Viktor <vppechko@gmail.com>
2021-04-29 21:31:31 +0300

    added new features

---
+++ WebApplication1/Data/Specifications/NewsSpecification.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication1.Data.Models;
+using WebApplication1.Data.Specifications.Base;
+
+namespace WebApplication1.Data.Specifications
+{
+    public class NewsSpecification : Specification<News>
+    {
+        public NewsSpecification() : base() { }
+
+        public NewsSpecification WhereIsCaruselItem()
+        {
+            AddWhere(order => order.FavImg != null);
+            return this;
+        }
+
+        public NewsSpecification SortById()
+        {
+            AddDescendingOrdering(order => order.Id);
+            return this;
+        }
+    }
+}
