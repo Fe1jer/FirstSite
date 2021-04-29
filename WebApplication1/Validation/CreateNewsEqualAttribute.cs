22ee531d7e3652c20f4064a85d48fcc3118ecdfd
Viktor <vppechko@gmail.com>
2021-04-29 22:11:19 +0300

    fixed

---
+++ WebApplication1/Validation/CreateNewsEqualAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Validation
+{
+    public class CreateNewsEqualAttribute : ValidationAttribute
+    {
+        public CreateNewsEqualAttribute()
+        {
+            ErrorMessage = "Заполните все поля!";
+        }
+        public override bool IsValid(object value)
+        {
+            CreateNewsViewModel model = value as CreateNewsViewModel;
+
+            if (model.IsCaruselNews == true && model.FavImg == null)
+            {
+                return false;
+            }
+            if (model.IsProductHref == true && model.ProductId == null)
+            {
+                return false;
+            }
+            else if (model.IsProductHref == false && model.Text == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
